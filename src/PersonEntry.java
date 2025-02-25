e3fd00929f0e67c8ecb5af3b39bf1f4076444cbe
kd <erm.melnik@gmail.com>
2025-02-25 19:10:35 +0100

    Initial commit

---
+++ src/PersonEntry.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+
+class PersonEntry {
+    private final Person person;
+    private int duplicates;
+
+    public PersonEntry(Person person, int duplicates) {
+        this.person = person;
+        this.duplicates = duplicates;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("PersonEntry {Person(\"%s\", %d), %d}", person.getName(), person.getAge(), duplicates);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonEntry that = (PersonEntry) o;
+        return duplicates == that.duplicates && Objects.equals(person, that.person);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(person);
+        result = 31 * result + duplicates;
+        return result;
+    }
+}

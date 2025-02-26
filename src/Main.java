fe4a7c23cb43e89665afd6c83cca8ec27bae9923
kd <erm.melnik@gmail.com>
2025-02-26 13:47:08 +0100

    Added countPerson_SortThenGetDuplicates() method.

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
         list1.add(null);
         list1.add(new Person("John", 22));
         list1.add(new Person("Jack", 12));
+        list1.add(new Person("Jack", 12));
+
 
         Set<PersonEntry> uniqueAndCounted = countPerson(list1);
         for (PersonEntry person : uniqueAndCounted) {
@@ -21,7 +23,13 @@ public class Main {
         }
     }
 
-    public static Set<PersonEntry> countPerson(List<Person> list) {
+    public static Set<PersonEntry> countPerson(List<Person> list){
+//        Set<PersonEntry> result =  countPerson_HashSetThenGetDuplicates(list);
+        Set<PersonEntry> result = countPerson_SortThenGetDuplicates(list);
+        return result;
+    }
+
+    public static Set<PersonEntry> countPerson_SortThenGetDuplicates(List<Person> list) {
         Set<PersonEntry> result = new HashSet<>();
 
         if (list == null || list.isEmpty()) return result;
@@ -31,7 +39,32 @@ public class Main {
                 i--;
             }
         }
-//        list.removeIf(Objects::isNull);
+
+        List<Person> editableList = new ArrayList<>(list);
+        editableList.sort(Comparator.comparing(Person::getName).thenComparing(Person::getAge));
+
+        int count = 1;
+        for (int i = 1; i <= editableList.size(); i++) {
+            if (i < editableList.size() && editableList.get(i).equals(editableList.get(i - 1))) {
+                count++;
+            } else {
+                result.add(new PersonEntry(editableList.get(i - 1), count));
+                count = 1;
+            }
+        }
+        return result;
+    }
+
+    public static Set<PersonEntry> countPerson_HashSetThenGetDuplicates(List<Person> list) {
+        Set<PersonEntry> result = new HashSet<>();
+
+        if (list == null || list.isEmpty()) return result;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == null) {
+                list.remove(i);
+                i--;
+            }
+        }
 
         Set<Person> uniqueSet = new HashSet<>(list);
 
@@ -46,6 +79,5 @@ public class Main {
         }
         return result;
     }
-
 }
 
